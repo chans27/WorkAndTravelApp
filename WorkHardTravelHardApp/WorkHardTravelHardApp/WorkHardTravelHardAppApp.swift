f0f694f5d04342a09c961d4bdc594e9f761983e6
YEACHAN <everever1275@gmail.com>
2022-12-28 03:23:58 +0900

    basic app

---
+++ WorkHardTravelHardApp/WorkHardTravelHardApp/WorkHardTravelHardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkHardTravelHardAppApp.swift
+//  WorkHardTravelHardApp
+//
+//  Created by YEACHAN OH on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct WorkHardTravelHardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
